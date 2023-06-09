c9dba5aa4b5671e8cd882d3b04cbeee7e1a02fee
R0-32 <wodrom@gmail.com>
2023-06-09 09:41:58 +0300

    Automatic commit: 2023-06-09 09:41:58.669952

--- Program.py
+++ Program.py
@@ -4,8 +4,8 @@ import datetime
 import configparser
 import notify2
 
-def extract_git_config(directory):
-    git_config_path = os.path.join(directory, '.git' , '.gitconfig')
+def extract_git_config():
+    git_config_path = os.path.expanduser("~/.gitconfig")
 
     if os.path.exists(git_config_path):
         config = configparser.ConfigParser()
@@ -14,25 +14,48 @@ def extract_git_config(directory):
         try:
             name = config.get('user', 'name')
             email = config.get('user', 'email')
-            url = config.get('remote "origin"', 'url')
-            return name, email, url
+            return name, email
         except (configparser.NoSectionError, configparser.NoOptionError):
             pass
 
-    return None, None, None
+    return None, None
+
+def extract_remote_origin_config(directory):
+    git_config_path = os.path.join(directory, '.git', 'config')
+
+    if os.path.exists(git_config_path):
+        config = configparser.ConfigParser()
+        config.read(git_config_path)
+
+        try:
+            url = config.get('remote "origin"', 'url')
+            return url
+        except (configparser.NoSectionError, configparser.NoOptionError):
+            pass
+
+    return None
 
 print("Запрос директории для отслеживания")
 directory = os.getcwd()
 
 print("# Извлечение информации из файла конфигурации Git")
-name, email, url = extract_git_config(directory)
+name, email = extract_git_config()
+url = extract_remote_origin_config(directory)
 
-if name is None or email is None or url is None:
-    print("Не удалось извлечь информацию из файла конфигурации Git.")
+if name is None or email is None:
+    print("Не удалось извлечь информацию о пользователе из файла конфигурации Git.")
 else:
     print("Имя: ", name)
     print("Email: ", email)
+
+if url is None:
+    print("Не удалось извлечь URL-адрес репозитория из файла конфигурации Git.")
+else:
     print("URL репозитория: ", url)
+
+if name is None or email is None or url is None:
+    print("Ошибка: не удалось извлечь необходимую информацию из файла конфигурации Git.")
+    exit()
 
 repo_path = os.getcwd()
 
@@ -41,6 +64,7 @@ try:
     print("# Репозиторий уже существует, можно продолжить работу")
 except git.exc.InvalidGitRepositoryError:
     print("# Репозиторий не существует, инициализируй, его")
+    exit()
 
 print("Добавляем все файлы в индекс")
 repo.git.add("--all")
@@ -52,7 +76,9 @@ commit_message = "Automatic commit: " + str(datetime.datetime.now())
 repo.index.commit(commit_message, author=author, committer=committer)
 
 print("# Создаем удаленную ссылку на репозиторий")
-remote = repo.create_remote("origin", url)
+remote = repo.remote(name="origin")
+if not remote.exists():
+    remote = repo.create_remote("origin", url)
 
 print("# Пушим изменения в репозиторий")
 remote.push(refspec="refs/heads/master")
@@ -61,6 +87,11 @@ print("# Инициализация модуля оповещений")
 notify2.init("Git Notifier")
 
 print("# Отслеживание изменений в директории")
+while True:
+    user_input = input("Введите команду (close для завершения): ")
+    if user_input == "close":
+        break
+
 for root, dirs, files in os.walk(repo_path):
     for file in files:
         file_path = os.path.join(root, file)
